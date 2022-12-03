368bad0a90fa36e16b3cfed1f64b70687ac90a58
KDH_Notebook <destiny1017@nate.com>
2022-12-03 11:09:25 +0900

    Initial commit

---
+++ src/main/java/com/example/studytest/StudytestApplication.java
@@ -0,0 +1,13 @@
+package com.example.studytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudytestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studytest/StudytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
